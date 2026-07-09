30722f38e21d32f99cb6fdcc07ad2ff0d49d61d3
SCHOOMP <grahamschomp2@gmail.com>
2026-07-09 17:39:55 -0500

    ingestion Script

--- ingestion/ercot_load.py
+++ ingestion/ercot_load.py
@@ -1,6 +1,27 @@
-## Sample Ingestion ##
-import gridstatus
-iso = gridstatus.Ercot()
-load = iso.get_load("today")        # DataFrame: Time, Load
-fuel = iso.get_fuel_mix("today")  # DataFrame: Time, Wind, Solar,# ...
-print(fuel)
+import os
+
+import psycopg2
+from psycopg2 import Error
+
+connection = None
+cursor = None
+
+try:
+    connection = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password= os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT")
+    )
+
+    print("Successfully connected to the database!")
+
+    cursor = connection.cursor()
+finally:
+    # 5. Clean up and close database objects
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+        print("PostgreSQL connection is safely closed.")
